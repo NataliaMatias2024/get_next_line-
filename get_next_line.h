7bd7c7a3d071135e7e96cc9c8f9d410cf929878c
Natalia <namatias@42sp.org.br>
2025-08-21 20:13:26 -0300

    Rascunho dos arquivos obrigatorios

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: namatias <namatias@42sp.org.br>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/21 20:05:07 by namatias          #+#    #+#             */
+/*   Updated: 2025/08/21 20:12:38 by namatias         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+
+#endif
