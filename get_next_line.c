92424c73c1123496094d889b0665a22200b54d66
Natalia <namatias@42sp.org.br>
2025-08-27 18:29:01 -0300

    Buffer_sizes grandes funcinando

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: namatias <namatias@42sp.org.br>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/26 16:35:47 by namatias          #+#    #+#             */
-/*   Updated: 2025/08/26 20:07:52 by namatias         ###   ########.fr       */
+/*   Updated: 2025/08/27 18:26:03 by namatias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,48 @@
 
 char *get_next_line(int fd)
 {
-	char *read_text;
-	ssize_t read_bytes;
+	static char *text;
+	ssize_t 	read_bytes;
+	char		*text_remainder;
+	char		*line;
 
-//Só retorna fd negativo em casos de erros
+//text_reminder vai ser responsavel por guardar todo o "resto" do texto após
+//retornar 1 linha. Iniciamos ela com NULL, assim se nao receber nada retorna NULL 
+	text_remainder = NULL;
+	
+//Em casos de erro o fd retorna negativo, por isso devemos validar
 	if (fd < 0)
 		return (NULL);
+
 //BUFFER_SIZE é uma variável estatica definida no nosso header
-	read_text = malloc (sizeof(char) * BUFFER_SIZE);
-	if (!read_text)
+//O malloc só deve ser feito quando o text ainda estiver vazio, ou seja, apenas na primeira chamada
+	if (!text)
+		text = malloc (sizeof(char) * BUFFER_SIZE);
+	if (!text)
 		return (NULL);
+
 //Read retorna o numero de bytes lidos com sucesso.
 //SE = -1, ocorreu algum erro. SE = 0 chegou ao fim do arquivo.
-	read_bytes = read(fd, read_text, BUFFER_SIZE);
+	read_bytes = read(fd, text, BUFFER_SIZE);
+	//printf("Read_bytes antes do if = %zd\n", read_bytes);
+	if (read_bytes > 0)
+	{
+		read_bytes = 0;
+		line = malloc ((BUFFER_SIZE + 1) * sizeof(char));
+		while (text[read_bytes] != '\n' && text[read_bytes] != '\0')
+		{
+			line[read_bytes] = text[read_bytes];
+			read_bytes++;
+		}
+		line[read_bytes] = '\n';
+		line[read_bytes + 1] = '\0';
 
 
-
-	printf("%s\n", read_text);
-	printf("%d\n", fd);
-	printf("%zd\n", read_bytes);
-	return (read_text);
+		text_remainder = ft_substr(text, (read_bytes + 1), (ft_strlen(text) - (read_bytes + 1)));
+		text = text_remainder;
+		return (line);
+	}
+	return (text_remainder);
 }
 
 int main()
@@ -42,10 +64,35 @@ int main()
 	char	*retorno;
 
 	fd = open("teste1.txt", O_RDONLY);
+
 	retorno = get_next_line(fd);
-	//printf("%s", retorno);
-	// get_next_line(fd);
-	// printf("%s", retorno);
+	printf("%s", retorno);
+	retorno = get_next_line(fd);
+	printf("%s", retorno);
+	retorno = get_next_line(fd);
+	printf("%s", retorno);
+	retorno = get_next_line(fd);
+	printf("%s", retorno);
+	retorno = get_next_line(fd);
+	printf("%s", retorno);
+	retorno = get_next_line(fd);
+	printf("%s", retorno);
+	retorno = get_next_line(fd);
+	printf("%s", retorno);
+	retorno = get_next_line(fd);
+	printf("%s", retorno);
+	retorno = get_next_line(fd);
+	printf("%s", retorno);
+	retorno = get_next_line(fd);
+	printf("%s", retorno);
+	retorno = get_next_line(fd);
+	printf("%s", retorno);
+	retorno = get_next_line(fd);
+	printf("%s", retorno);
+
+//daqui pra frente tem que devolver NULL
+	retorno = get_next_line(fd);
+	printf("%s", retorno);
 
 	close(fd);
 }
