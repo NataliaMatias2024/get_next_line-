055465e474de7a9c392e643ea37e2c441f1eebf1
Natalia <namatias@42sp.org.br>
2025-08-27 16:38:09 -0300

    Atualizando prototipos necessarios

--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: namatias <namatias@42sp.org.br>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/21 20:05:07 by namatias          #+#    #+#             */
-/*   Updated: 2025/08/26 20:11:05 by namatias         ###   ########.fr       */
+/*   Updated: 2025/08/27 16:35:02 by namatias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
+ 
 //Para usar o write
 # include <unistd.h>
 //Para usar funções com alocação de memoria como Malloc, Calloc e Free
@@ -23,9 +23,12 @@
 # include <stdio.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 30
+#  define BUFFER_SIZE 2
 # endif
 
 char	*get_next_line(int fd);
+int		ft_strlen(const char *s);
+char	*ft_strdup(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
