dd43b69c1f96765d1e0744e16b05da5f0188d577
Natalia <namatias@42sp.org.br>
2025-08-30 14:04:01 -0300

    Refazendo a função

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: namatias <namatias@42sp.org.br>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/26 16:35:47 by namatias          #+#    #+#             */
-/*   Updated: 2025/08/30 13:28:16 by namatias         ###   ########.fr       */
+/*   Updated: 2025/08/30 14:01:06 by namatias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,15 @@
 
 char *get_next_line(int fd)
 {
+	ssize_t	bytes_reads;
+	char	*text;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
 	
+	text = malloc(BUFFER_SIZE * sizeof(char *));
+	bytes_reads = read(fd, text, BUFFER_SIZE);
+	return (text);
 }
 
 int main()
@@ -23,6 +31,7 @@ int main()
 	char	*retorno;
 
 	fd = open("teste1.txt", O_RDONLY);
+	//fd = -2;
 
 	retorno = get_next_line(fd);
 	printf("%s", retorno);
