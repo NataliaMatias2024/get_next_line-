136d1e14ec431bd563af8fec9a56ee79dcb8d2ef
Natalia <namatias@42sp.org.br>
2025-08-30 13:29:48 -0300

    Adiciona mais uma func auxiliar

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: namatias <namatias@42sp.org.br>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/27 14:03:13 by namatias          #+#    #+#             */
-/*   Updated: 2025/08/27 16:17:28 by namatias         ###   ########.fr       */
+/*   Updated: 2025/08/30 09:59:08 by namatias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,4 +66,46 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	}
 	ptr[i] = '\0';
 	return (ptr);
+}
+
+int	protect(char const *s1, char const *s2)
+{
+	int		size_s1;
+	int		size_s2;
+	int		size;
+
+	if (!s1)
+		size_s1 = 0;
+	else
+		size_s1 = ft_strlen(s1);
+	if (!s2)
+		size_s2 = 0;
+	else
+		size_s2 = ft_strlen(s2);
+	size = size_s1 + size_s2;
+	return (size);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	int		i;
+	int		j;
+
+	if (!s1 && !s2)
+		return (NULL);
+	str = malloc ((protect(s1, s2) + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	i = 0;
+	j = 0;
+	if (s1)
+		while (s1[i])
+			str[j++] = s1[i++];
+	i = 0;
+	if (s2)
+		while (s2[i])
+			str[j++] = s2[i++];
+	str[j] = '\0';
+	return (str);
 }
