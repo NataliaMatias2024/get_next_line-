b844df061e2499e36ee32131610a2d263904638a
namatias <namatias@student.42sp.org.br>
2025-09-05 22:20:03 -0300

    Func. atualizadas e com norminette

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: namatias <namatias@42sp.org.br>            +#+  +:+       +#+        */
+/*   By: namatias <namatias@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/27 14:03:13 by namatias          #+#    #+#             */
-/*   Updated: 2025/08/31 16:51:50 by namatias         ###   ########.fr       */
+/*   Updated: 2025/09/05 22:02:32 by namatias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 
 int	ft_strlen(const char *s)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
+	if (!s)
+		return (0);
 	while (s[i])
 		i++;
 	return (i);
@@ -68,33 +70,19 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	return (ptr);
 }
 
-int	protect(char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
 	int		size_s1;
 	int		size_s2;
-	int		size;
-
-	if (!s1)
-		size_s1 = 0;
-	else
-		size_s1 = ft_strlen(s1);
-	if (!s2)
-		size_s2 = 0;
-	else
-		size_s2 = ft_strlen(s2);
-	size = size_s1 + size_s2;
-	return (size);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
 	char	*str;
 	int		i;
 	int		j;
 
+	size_s1 = ft_strlen(s1);
+	size_s2 = ft_strlen(s2);
 	if (!s1 && !s2)
 		return (NULL);
-	str = malloc ((protect(s1, s2) + 1) * sizeof(char));
+	str = malloc ((size_s1 + size_s2 + 1) * sizeof(char));
 	if (!str)
 		return (NULL);
 	i = 0;
