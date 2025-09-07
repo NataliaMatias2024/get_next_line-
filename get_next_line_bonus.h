c337c9cccb320e45c99537271056e9cef067e5d9
Natalia <namatias@42sp.org.br>
2025-09-07 15:28:56 -0300

    Criando arquivos Bonus

---
+++ get_next_line_bonus.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: namatias <namatias@42sp.org.br>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/07 14:28:10 by namatias          #+#    #+#             */
+/*   Updated: 2025/09/07 15:13:51 by namatias         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+# ifndef MAX_FDS
+#  define MAX_FDS 1024
+# endif
+
+char	*get_next_line(int fd);
+int		ft_strlen(const char *s);
+char	*ft_strdup(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+
+#endif
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,115 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: namatias <namatias@42sp.org.br>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/07 15:14:54 by namatias          #+#    #+#             */
+/*   Updated: 2025/09/07 15:28:07 by namatias         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+int	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(const char *s)
+{
+	int		i;
+	char	*dup;
+
+	i = ft_strlen(s);
+	dup = malloc ((i + 1) * sizeof(const char));
+	if (!dup)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		dup[i] = s[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	len_s;
+	size_t	i;
+	char	*ptr;
+
+	if (s == NULL)
+		return (ft_strdup(""));
+	len_s = ft_strlen(s);
+	if (start >= len_s)
+		return (ft_strdup(""));
+	i = 0;
+	while (i < len && s[start + i] != '\0')
+		i++;
+	ptr = malloc ((i + 1) * sizeof(char));
+	if (!ptr)
+		return (NULL);
+	i = 0;
+	while (i < len && s[start + i] != '\0')
+	{
+		ptr[i] = s[start + i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		size_s1;
+	int		size_s2;
+	char	*str;
+	int		i;
+	int		j;
+
+	size_s1 = ft_strlen(s1);
+	size_s2 = ft_strlen(s2);
+	if (!s1 && !s2)
+		return (NULL);
+	str = malloc ((size_s1 + size_s2 + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	i = 0;
+	j = 0;
+	if (s1)
+		while (s1[i])
+			str[j++] = s1[i++];
+	i = 0;
+	if (s2)
+		while (s2[i])
+			str[j++] = s2[i++];
+	str[j] = '\0';
+	return (str);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (1)
+	{
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		if (s[i] == '\0')
+			break ;
+		i++;
+	}
+	return (NULL);
+}
